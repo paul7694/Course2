615f195302fcdcaff558c1c06399e4849c4cbe5d
Piklu Paul <paulpiklu123@gmail.com>
2020-06-14 11:30:07 +0530

    Assignment-2

--- src/App.js
+++ src/App.js
@@ -1,42 +1,21 @@
 import React, {Component} from 'react';
-import {Navbar, NavbarBrand} from 'reactstrap';
+import Main from './components/MainComponent';
 import './App.css';
-import Menu from './components/MenuComponent';
-import { DISHES } from "./shared/dishes";
+import { BrowserRouter } from 'react-router-dom';
 
 
 class App extends Component {
-  /**
-   * Constructor
-   * @param props properties
-   */
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      dishes: DISHES
-    };
-  }
-
-  /**
-   * All React components must
-   * implement the render() method
-   */
   render() {
     return (
-      <div>
-        {/* navbar component */}
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        {/* use of a React component we've created */}
-        {/* make this component's state available to child components via props*/}
-        <Menu dishes={this.state.dishes}/>
+      <BrowserRouter>
+      <div className="App">
+        <Main />
       </div>
+    </BrowserRouter>
     );
   }
 }
+  
 
 export default App;
---
+++ src/components/MainComponent.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import Menu from './MenuComponents';
+import DishDetail from './DishdetailComponent';
+import { DISHES } from '../shared/dishes';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
+import { COMMENTS } from '../shared/comments';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import Contact from './ContactComponent';
+import About from './AboutComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+class Main extends Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+          dishes: DISHES,
+          comments: COMMENTS,
+          leaders: LEADERS,
+          promotions: PROMOTIONS,
+          selectedDish: null
+      };
+    }
+    onDishSelect(dishId) {
+      this.setState({ selectedDish: dishId});
+    }
+    render() {
+      const HomePage = () => {
+        return(
+          <Home 
+            dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+            promotion={this.state.promotions.filter((promotion) => promotion.featured)[0]}
+            leader={this.state.leaders.filter((leader) => leader.featured)[0]} 
+          />
+        );
+      }
+      const DishWithId = ({match}) => {
+        return (
+          <DishDetail 
+            dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+        );
+      }
+      return (
+        <div>
+          <Header />
+          <Switch>
+            <Route path='/home' component={HomePage}/>
+            <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+            <Route path="/menu/:dishId" component={DishWithId}/>
+            <Route path='/contactus' component={Contact} />
+            <Route path='/aboutus' component={() => <About leaders={this.state.leaders} />}/>
+            <Redirect to="/home" />
+          </Switch>
+          <Footer />
+        </div>
+      );
+    }
+  }
+  export default Main;
